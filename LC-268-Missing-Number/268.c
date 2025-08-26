4d7235b7aed5bbdd208d66358c24ea4943829f64
Evan-X-Hu <exh4wrk@gmail.com>
2025-08-26 02:37:08 -0400

    I'm really happy with this, my algorithm worked. But of course this was an easy problem, the others will probably, and hopefully be much more difficult

--- LC-268-Missing-Number/268.c
+++ LC-268-Missing-Number/268.c
@@ -4,7 +4,7 @@
 
 int* convertToIntArr(char* str, int size);
 int findMissingNum(int* numArr, int len);
-
+void printArray(int* arr, int n);
 
 int main(void) {
     // Initialize input fields
@@ -26,6 +26,9 @@ int main(void) {
     numArr = convertToIntArr(buffer, len);
 
     // Now perform the algorithm to find the missing number
+    int missingNum = findMissingNum(numArr, len);
+
+    fprintf(stdout, "Missing number: %d\n", missingNum);
 }
 
 int* convertToIntArr(char* str, int size) {
@@ -46,5 +49,44 @@ int* convertToIntArr(char* str, int size) {
 }
 
 int findMissingNum(int* numArr, int len) {
+    // First initialize an empty array of size len
+    int numsMap[len];
+
+    // Second iterate through all numbers in numArr and map a 1 to the index of that number
+    // Something weird happens here when the numbers aren't sequenced
+    // Im unsure what it is but I know that if I initialize everything to 0 it should solve the issue
+    // Note: Come back to this to learn about the issue behind this
+    for(int i=0; i<len; i++) {
+        numsMap[i] = 0;
+    }
+    // printArray(numsMap, len);
     
+    for(int i=0; i<len; i++) {
+        //fprintf(stdout, "TEST: %d\n", numArr[i]);
+        int curNum = numArr[i];
+        numsMap[curNum] = 1;
+    }
+
+    // Print this array for testing
+    // printArray(numsMap, len);
+
+    // So this works, I just need to make a note that the right edge case is when all the values are mapped to 1
+    // I mean this is a better and faster solution then sorting the array and looking for a jump
+
+    // Iterate through the map and return the index with a 0, if all 1, then we return len
+    for(int i=0; i<len; i++) {
+        if(numsMap[i] == 0) {
+            return i;
+        }
+    }
+
+    return len;
+}
+
+// Code pasted from ChatGPT, I got lazy, it happens
+void printArray(int* arr, int n) {
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
